3b5c96dec013232a0991b462650eff7874063ec8
ggermashev <ggermashev@gmail.com>
2024-03-13 15:39:13 +0300

    watch events from inputs

--- js/spinner.js
+++ js/spinner.js
@@ -1,8 +1,66 @@
 window.addEventListener('load', () => {
+    // const emiter = new EventEmitter()
+
     const spinners = document.querySelectorAll(".spinner")
     const spinnerValues = document.querySelectorAll(".spinner-value")
     const spinnerAnimations = document.querySelectorAll(".spinner-animation")
     const spinnerVisibilities = document.querySelectorAll(".spinner-visibility")
 
-    
+    spinners.forEach(spinner => {
+        const id = spinner.getAttribute("id")
+        spinner.addEventListener(`spinner-value-${id}`, (el) => {
+            console.log("value", el.value)
+        })
+        spinner.addEventListener(`spinner-animation-${id}`, (el) => {
+            console.log("animation", el.value)
+        })
+        spinner.addEventListener(`spinner-visibility-${id}`, (el) => {
+            console.log("visibility", el.value)
+        })
+    })
+
+    spinnerValues.forEach(input => {
+        const spinnerId = input.getAttribute("spinner_id")
+        const spinner  = document.getElementById(spinnerId)
+
+        input.addEventListener('change', (e) => {
+            const event = new Event(`spinner-value-${spinnerId}`)
+            event.value = e.target.value
+            spinner.dispatchEvent(event)
+        })
+    })
+
+    spinnerAnimations.forEach(checkbox => {
+        const spinnerId = checkbox.getAttribute("spinner_id")
+        const spinner  = document.getElementById(spinnerId)
+
+        checkbox.addEventListener('change', (e) => {
+            const event = new Event(`spinner-animation-${spinnerId}`)
+            event.value = e.target.checked
+            spinner.dispatchEvent(event)
+        })
+    })
+
+    spinnerVisibilities.forEach(checkbox => {
+        const spinnerId = checkbox.getAttribute("spinner_id")
+        const spinner  = document.getElementById(spinnerId)
+
+        checkbox.addEventListener('change', (e) => {
+            const event = new Event(`spinner-visibility-${spinnerId}`)
+            event.value = e.target.checked
+            spinner.dispatchEvent(event)
+        })
+    })
 })
+
+function drawSpinner() {
+
+}
+
+function rotateSpinner() {
+
+}
+
+function setSpinerVisibility() {
+
+}
