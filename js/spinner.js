44d453cc1d221b2b7185ee90976651f6c0610769
ggermashev <ggermashev@gmail.com>
2024-03-13 14:48:47 +0300

    init

---
+++ js/spinner.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    const spinners = document.querySelectorAll(".spinner")
+    const spinnerValues = document.querySelectorAll(".spinner-value")
+    const spinnerAnimations = document.querySelectorAll(".spinner-animation")
+    const spinnerVisibilities = document.querySelectorAll(".spinner-visibility")
+
+    
+})
